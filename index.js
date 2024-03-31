03c44ac1c7d8e0924ce7bc16d2617275dfde03bd
bushraAfsar1 <126601448+bushraAfsar1@users.noreply.github.com>
2024-03-31 15:59:51 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let yourPin = 2222;
+let yourAmount = 20000;
+console.log('Welcome to B&A ATM');
+// console.log(yourPin);
+let atm = await inquirer.prompt([
+    {
+        name: 'pin',
+        type: 'number',
+        message: 'Enter your pin Code',
+    }
+]);
+if (yourPin === atm.pin) {
+    let operationsMedhod = await inquirer.prompt([
+        {
+            name: 'operations',
+            type: 'list',
+            message: 'Select your Operation method',
+            choices: ['Withdraw', 'Check Balance'],
+        }
+    ]);
+    if (operationsMedhod.operations === 'Withdraw') {
+        let withdrawMedhod = await inquirer.prompt([
+            {
+                name: 'amountWthdraw',
+                type: 'list',
+                message: 'Select your Withdraw method',
+                choices: ['Enter Amount', ' Fast Cash'],
+            }
+        ]);
+        if (withdrawMedhod.amountWthdraw === 'Enter Amount') {
+            let withdrawBalance = await inquirer.prompt([
+                {
+                    name: 'balance',
+                    type: 'number',
+                    message: 'enter Balance',
+                }
+            ]);
+            if (withdrawBalance.balance <= yourAmount) {
+                console.log('Collect your Amount ', `now your current balance is ${yourAmount - withdrawBalance.balance}`);
+            }
+            else {
+                console.log('insufficiant balance');
+            }
+            ;
+        }
+        else {
+            let fastCashMedhod = await inquirer.prompt([
+                {
+                    name: 'fastCash',
+                    type: 'list',
+                    message: 'Select amount',
+                    choices: ['1000', '3000', '5000'],
+                }
+            ]);
+            if (fastCashMedhod.fastCash <= yourAmount) {
+                console.log('Collect your Amount ', `now your current balance is ${yourAmount - fastCashMedhod.fastCash}`);
+            }
+            else {
+                console.log('insufficiant balance');
+            }
+        }
+        ;
+    }
+    else {
+        console.log(`Your Current Amount ${yourAmount}`);
+    }
+    ;
+}
+else {
+    console.log("Incorrect Pin");
+}
+;
+//  console.log(atm);
